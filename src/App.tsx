b97bc7b1d757c2603a073e1e3d63715fa8c46bc1
Saghar2002 <Sagharrigi81@gmail.com>
2025-06-10 00:44:57 +0330

    🎉 Todo-app
    start todo-app project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './design/main.css'
+
+function App() {
+
+
+  return (
+    <div  className="bg-black text-white p-6 text-center">
+      <h1 className="bg-green-600  justify-center text-red">heloooo</h1>
+    </div>
+  )
+}
+
+export default App
